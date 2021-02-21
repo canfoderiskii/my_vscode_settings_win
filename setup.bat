e728241b1e2cd45596d758066f40a2767773208a
Canfoderiskii <wch007@msn.com>
2021-02-21 23:28:04 +0800

    setup support for TabNine extension

--- setup.bat
+++ setup.bat
@@ -15,6 +15,9 @@ set PORTABLE_CODE_FOLDER=%~dp0\Code
 set APP_CODE_FOLDER=%APPDATA%\Code
 set INSIDERS_APP_CODE_FOLDER="%APPDATA%\Code - Insiders"
 
+set PORTABLE_TABNINE_FOLDER=%~dp0\TabNine
+set APP_TABNINE_FOLDER=%APPDATA%\TabNine
+
 :: if folders exist, delete them
 
 if exist %USERPROFILE_DOTVSCODE_FOLDER% (
@@ -37,6 +40,11 @@ if exist %INSIDERS_APP_CODE_FOLDER% (
     rmdir /s /q %INSIDERS_APP_CODE_FOLDER%
 )
 
+if exist %APP_TABNINE_FOLDER% (
+    @echo "WARNING: TabNine folder exist inside APPDATA, deleting.."
+    rmdir /s /q %APP_TABNINE_FOLDER%
+)
+
 :: delete folders that already exist inside USERPROFILE or APPDATA
 call %PORTABLE_ROOT%\_clean_link.bat
 
@@ -46,6 +54,7 @@ mklink /d %USERPROFILE_DOTVSCODE_FOLDER% %PORTABLE_DOTVSCODE_FOLDER%
 mklink /d %APP_CODE_FOLDER% %PORTABLE_CODE_FOLDER%
 mklink /d %INSIDERS_USERPROFILE_DOTVSCODE_FOLDER% %PORTABLE_DOTVSCODE_FOLDER%
 mklink /d %INSIDERS_APP_CODE_FOLDER% %PORTABLE_CODE_FOLDER%
+mklink /d %APP_TABNINE_FOLDER% %PORTABLE_TABNINE_FOLDER%
 
 :: check if folders are created, due to FAT32 does not support link
 
